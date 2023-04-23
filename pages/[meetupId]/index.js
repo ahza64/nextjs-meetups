669b0d98604de26044009459736c332c2381badc
ahza64 <justinmichaelpettit@gmail.com>
2023-04-23 15:14:44 -0700

    imperative navigation, meetup detail and css

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1542213458-267646c929e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
+      title="First meetup"
+      address="123 some road some city"
+      description="the first meetup"
+    />
+  )
+}
+
+export default MeetupDetails
